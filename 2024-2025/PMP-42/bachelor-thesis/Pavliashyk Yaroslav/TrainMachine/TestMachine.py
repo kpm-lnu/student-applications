bbe20af197ebd8ecbbb263b84b8c4b030440287a
Yaroslav Pavliashyk <112720203+caynox@users.noreply.github.com>
2025-06-10 23:07:27 +0300

    Update TestMachine.py

--- 2024-2025/PMP-42/bachelor-thesis/Pavliashyk Yaroslav/TrainMachine/TestMachine.py
+++ 2024-2025/PMP-42/bachelor-thesis/Pavliashyk Yaroslav/TrainMachine/TestMachine.py
@@ -5,23 +5,23 @@ from PIL import Image
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 
-# === Шляхи ===
+
 MODEL_PATH = r"D:\Programming\Diploma\TrainMachine\best_emotion_model_valacc.keras"
 TEST_DIR = r"D:\Programming\Diploma\NewDatasetDirectory\test"
 OUTPUT_DIR = r"D:\Programming\Diploma\NewDatasetDirectory\test_result"
 IMG_SIZE = (128, 128)
 
-# === Класи емоцій ===
+
 EMOTION_LABELS = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
 
-# === Завантаження моделі ===
+
 model = load_model(MODEL_PATH)
 
-# === Створення директорій для результатів ===
+
 for label in EMOTION_LABELS:
     os.makedirs(os.path.join(OUTPUT_DIR, label), exist_ok=True)
 
-# === Класифікація зображень ===
+
 for filename in os.listdir(TEST_DIR):
     if not filename.lower().endswith(('.jpg', '.jpeg', '.png')):
         continue
@@ -29,7 +29,7 @@ for filename in os.listdir(TEST_DIR):
     path = os.path.join(TEST_DIR, filename)
     try:
         img = Image.open(path).convert("RGB")
-        img = img.resize(IMG_SIZE, Image.Resampling.LANCZOS)  # ✅ Новий антиаліасинг
+        img = img.resize(IMG_SIZE, Image.Resampling.LANCZOS)  
         arr = img_to_array(img) / 255.0
         arr = np.expand_dims(arr, axis=0)
 
@@ -37,7 +37,7 @@ for filename in os.listdir(TEST_DIR):
         label_index = np.argmax(prediction)
         label_name = EMOTION_LABELS[label_index]
 
-        # === Зберегти результат ===
+
         dest_path = os.path.join(OUTPUT_DIR, label_name, filename)
         shutil.copy(path, dest_path)
         print(f"✅ {filename} → {label_name}")
