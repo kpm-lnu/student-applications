9bee2b3642171c8fa73831253a004b4f2d31ea50
Yaroslav Pavliashyk <112720203+caynox@users.noreply.github.com>
2025-06-10 23:05:55 +0300

    Update Minimalparametrs.py

--- 2024-2025/PMP-42/bachelor-thesis/Pavliashyk Yaroslav/PreparingForTraining/Minimalparametrs.py
+++ 2024-2025/PMP-42/bachelor-thesis/Pavliashyk Yaroslav/PreparingForTraining/Minimalparametrs.py
@@ -1,7 +1,6 @@
 import os
 from PIL import Image
 
-# === Вкажи шлях до директорії з зображеннями ===
 #ROOT_DIR = r"D:\Programming\Diploma\datasets\Oahega"
 #ROOT_DIR = r"D:\Programming\Diploma\datasets\combined_dataset"
 ROOT_DIR = r"D:\Programming\Diploma\NewDatasetDirectory\train"
@@ -9,9 +8,9 @@ ROOT_DIR = r"D:\Programming\Diploma\NewDatasetDirectory\train"
 min_width = float('inf')
 min_height = float('inf')
 image_count = 0
-small_image_count = 0  # Лічильник маленьких зображень
+small_image_count = 0 
 
-# === Обхід усіх зображень рекурсивно ===
+
 for root, _, files in os.walk(ROOT_DIR):
     for file in files:
         if file.lower().endswith(('.jpg', '.jpeg', '.png')):
@@ -22,18 +21,15 @@ for root, _, files in os.walk(ROOT_DIR):
                     image_count += 1
                     print(f"[{image_count}] 📷 {file} → {width}x{height}")
 
-                    # Мінімальні розміри
                     min_width = min(min_width, width)
                     min_height = min(min_height, height)
 
-                    # Підрахунок маленьких зображень
                     if width < 128 or height < 128:
                         small_image_count += 1
 
             except Exception as e:
                 print(f"⚠️ Проблема з файлом {image_path}: {e}")
 
-# === Результати ===
 if image_count > 0:
     print("\n📊 Статистика:")
     print(f"🔢 Загальна кількість зображень: {image_count}")
